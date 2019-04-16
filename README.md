aa4d8b85f4357aa805a0cf1dbdc55b9898f540e4
Oscar Levin <oscar.levin@gmail.com>
2019-04-15 22:40:22 -0600

    Fix tables, add to readme

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # Pandoc to PreTeXt
 
-A first stab at creating a Lua writer to convert anything pandoc reads to PreTeXt.  The writer will be based extensively on [pandoc-jats]{https://github.com/mfenner/pandoc-jats}.
+A first stab at creating a Lua writer to convert anything [Pandoc]{https://pandoc.org/} reads to PreTeXt.  The writer will be based loosely on [pandoc-jats]{https://github.com/mfenner/pandoc-jats}.
 
-The goal is primarily to streamline the conversion of latex (or even word) files into PreTeXt.  If successful though, this might make for a reasonable workflow for PreTeXt newbies: write in pandoc's markdown or whatever they are used to, then convert to PreTeXt to include in a book.
+The goal is primarily to streamline the conversion of latex (or even MS Word) files into PreTeXt.  If successful though, this might make for a reasonable workflow for PreTeXt newcomers: write in Pandoc's markdown, or whatever they are used to, then convert to PreTeXt to include in a book.
 
-I expect a fair amount of post-precessing necessary, but we will see.
+I expect a fair amount of post-processing necessary, but we will see.
 
-## Another eventual goal
+## Installation
+Just download the file `pretext.lua` and put it in a convenient location. Pandoc includes a lua interpreter, so lua need not be installed separately. You might need at least Pandoc version 1.13, released August 2014 (this release adds `--template` support for custom writers, which might be added soon).
+
+## Usage
+To convert the markdown file `manual.md` into the PreTeXt file `manual.ptx`, use the following command:
+
+```
+pandoc examples/example1.md -t pretext.lua -o manual.ptx
+```
+
+Of course you can (might need to) specify a path to the `pretext.lua` file, depending on where it is located.
+
+## Issues
+
+Currently, there are the following known issues with the output:
+
+1. Sections work as expected, but the `<introduction>` is missing around blocks where it should be present.
+2. No support for theorems/definitions/etc.  This might be impossible, although there is a amsthm "filter" available.  I don't think that reads anything by custom YAML that an author would specifically put in a markdown file though.
+3. Not sure what to do with citations.
+4. Others?
+
+Please report any issues.
+
+## Future work
 
 Need to test this now, but how good is pandoc and converting PreTeXt generated LaTeX?  Perhaps a simplified xsl sheet could produce nicer LaTeX for pandoc, which could then be converted to other formats (like for slides).  
 
--- pretext.lua
+++ pretext.lua
@@ -1,9 +1,5 @@
--- This is a sample custom writer for pandoc.  It produces output
--- that is very similar to that of pandoc's HTML writer.
--- There is one new feature: code blocks marked with class 'dot'
--- are piped through graphviz and images are included in the HTML
--- output using 'data:' URLs. The image format can be controlled
--- via the `image_format` metadata field.
+-- This is a PreTeXt custom writer for pandoc,
+-- based loosely on the JATS custom writter: https://github.com/mfenner/pandoc-jats. 
 --
 -- Invoke with: pandoc -t pretext.lua
 --
@@ -21,29 +17,9 @@ local utils = require 'pandoc.utils'
 -- the document which is going to be written. It can be used to
 -- configure the writer.
 local meta = PANDOC_DOCUMENT.meta
--- local blocks = PANDOC_DOCUMENT.blocks
--- local h = utils.hierarchicalize(blocks)
--- dumpit(h[1].contents[2].contents)
--- print(hierarchy[2].level)
--- dumpit(hierarchy[2].attr)
--- dumpit(hierarchy[1].contents[1].contents)
--- print(table.concat(hierarchy[1].numbering, '.'))
--- dumpit(block)
 
--- local h = pandoc.utils.hierarchicalize(pandoc.Pandoc)
-
--- dumpit(h[1].contents)
 -- global variable to keep track of indent level:
 indents = 1
-
--- function Pandoc(doc)
---   local elements = pandoc.utils.hierarchicalize(doc.blocks)
---   print("it's working")
---   for _,e in ipairs(elements) do
---     print(e,t)
---   end
--- end
-
 
 -- This function is called once for the whole document. Parameters:
 -- body is a string, metadata is a table, variables is a table.
@@ -78,10 +54,6 @@ function Doc(body, metadata, variables)
 end
 
 
-
-
-
-
 -- Chose the image format based on the value of the
 -- `image_format` meta value.
 local image_format = meta.image_format
@@ -96,7 +68,6 @@ local image_mime_type = ({
   })[image_format]
   or error("unsupported image format `" .. img_format .. "`")
   
-
 -- Character escaping
 -- (might want to remove the quotes, double check pretext)
 local function escape(s, in_attribute)
@@ -121,31 +92,28 @@ end
 -- Helper function to convert an attributes table into
 -- a string that can be put into HTML tags.
 local function attributes(attr)
-  -- dumpit(attr)
   local attr_table = {}
   for x,y in pairs(attr) do
     if y and y ~= "" then
-      table.insert(attr_table, ' xml:' .. x .. '="' .. escape(y,true) .. '"')
+      if x == "id" then
+        table.insert(attr_table, ' xml:id="' .. escape(y,true)..'"')
+      else
+        table.insert(attr_table, ' '..x .. '="' .. escape(y,true) .. '"')
+      end
     end
   end
   return table.concat(attr_table)
 end
-
--- -- Table to store footnotes, so they can be included at the end.
--- local notes = {}
 
 -- Blocksep is used to separate block elements.
 function Blocksep()
   return "\n\n"
 end
 
-
-
 -- The functions that follow render corresponding pandoc elements.
 -- s is always a string, attr is always a table of attributes, and
 -- items is always an array of strings (the items in a list).
 -- Comments indicate the types of other variables.
-
 
 function Str(s)
   return escape(s)
@@ -191,10 +159,6 @@ function Strikeout(s)
 end
 
 function Link(s, src, tit, attr)
-  -- dumpit(s)
-  -- dumpit(src)
-  -- dumpit(tit)
-  -- dumpit(attr)
   if string.sub(src, 1, 1) == "#" then
     return '<xref ref="'..escape(string.sub(src, 2))..'" />'
   else
@@ -231,17 +195,17 @@ function Note(s)
 end
 
 function Span(s, attr)
-  -- return s
  return "<span" .. attributes(attr) .. ">" .. s .. "</span>"
 end
 
-function RawInline(format, str)
-  if format == "html" then
-    return str
-  else
-    return ''
-  end
-end
+-- -- I don't know what this does:
+-- function RawInline(format, str)
+--   if format == "html" then
+--     return str
+--   else
+--     return ''
+--   end
+-- end
 
 function Cite(s, cs)
   local ids = {}
@@ -353,66 +317,68 @@ end
 -- widths is an array of floats, headers is an array of
 -- strings, rows is an array of arrays of strings.
 function Table(caption, aligns, widths, headers, rows)
+  local tabs = string.rep("\t", indents)
+  local tabsp = string.rep("\t", indents+1)
+  local tabspp = string.rep("\t", indents+2)
   local buffer = {}
   local function add(s)
     table.insert(buffer, s)
   end
-  add("<table>")
-  if caption ~= "" then
-    add("<caption>" .. caption .. "</caption>")
-  end
+  add(tabs.."<table>")
+  -- if caption ~= "" then -- tabules need captions always
+    add(tabsp.."<caption>" .. caption .. "</caption>")
+  -- end
   if widths and widths[1] ~= 0 then
     for _, w in pairs(widths) do
       add('<col width="' .. string.format("%.0f%%", w * 100) .. '" />')
     end
   end
+  add(tabsp..'<tabular>')
   local header_row = {}
   local empty_header = true
   for i, h in pairs(headers) do
     local align = html_align(aligns[i])
-    table.insert(header_row,'<th align="' .. align .. '">' .. h .. '</th>')
+    table.insert(header_row, tabspp..'<cell halign="' .. align .. '">' .. h .. '</cell>')
     empty_header = empty_header and h == ""
   end
   if empty_header then
     head = ""
   else
-    add('<tr class="header">')
+    add(tabsp..'<row header="yes">')
     for _,h in pairs(header_row) do
       add(h)
     end
-    add('</tr>')
+    add(tabsp..'</row>')
   end
   local class = "even"
   for _, row in pairs(rows) do
     class = (class == "even" and "odd") or "even"
-    add('<tr class="' .. class .. '">')
+    add(tabsp..'<row class="' .. class .. '">')
     for i,c in pairs(row) do
-      add('<td align="' .. html_align(aligns[i]) .. '">' .. c .. '</td>')
+      add(tabspp..'<cell halign="' .. html_align(aligns[i]) .. '">' .. c .. '</cell>')
     end
-    add('</tr>')
+    add(tabsp..'</row>')
   end
-  add('</table>')
+  add(tabsp..'</tabular>\n'..tabs..'</table>')
   return table.concat(buffer,'\n')
 end
 
 function RawBlock(format, str)
-  return "<cd>\n\t" .. str .. "\n</cd>"
+  return "<cd>\n" .. str .. "\n</cd>"
 end
 
 -- We temporarily keep <h1> etc wrapping titles of sections.  These will be converted to a hierarchy of sections in the last step (Doc).
 -- lev is an integer, the header level.
 function Header(lev, s, attr)
- -- return "<h" .. lev .. ">" .. s .. "</h" .. lev .. ">"
- indents = lev+1
- local tabs = string.rep("\t", indents-1)
- local tabsp = string.rep("\t", indents)
- return tabs.."<h"..lev..attributes(attr)..">\n"..tabsp.."<title>"..s.."</title>"
- -- return "<title>" .. s .. "</title>"
+  indents = lev+1
+  local tabs = string.rep("\t", indents-1)
+  local tabsp = string.rep("\t", indents)
+  return tabs.."<h"..lev..attributes(attr)..">\n"..tabsp.."<title>"..s.."</title>"
 end
 
+-- This might never do anything; but if it does, I can use it!
 function Div(s, attr)
-  -- return s .. "\n"
- return "<div" .. attributes(attr) .. ">\n" .. s .. "</div>"
+  return "<div" .. attributes(attr) .. ">\n" .. s .. "</div>"
 end
 
 
